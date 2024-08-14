23c552bf46a0f592129e5bb4f23b9b483e709a8b
MarharytaSom <158225902+MarharytaSom@users.noreply.github.com>
2024-08-14 21:05:18 +0200

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Feedback from './components/Feedback/Feedback';
 import Options from './components/Options/Options';
 import Notification from './components/Notification/Notification';
-
+import Description from './components/Description/Description'
 
 import styles from './components/Feedback/Feedback';
 
@@ -51,7 +51,7 @@ const App = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Sip Happens Café</h1>
-      <p className={styles.description}>Please leave your feedback about our service by selecting one of the options below.</p>
+      <Description />
       <Options onLeaveFeedback={updateFeedback} onResetFeedback={resetFeedback} totalFeedback={totalFeedback} />
       {totalFeedback > 0 ? (
         <>
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,13 @@
 import styles from './Feedback.module.css';
 
-const Feedback = ({ feedback }) => {
+const Feedback = ({ feedback, totalFeedback, positiveFeedbackPercentage }) => {
   return (
     <div className={styles.feedback}>
       <p>Good: {feedback.good}</p>
       <p>Neutral: {feedback.neutral}</p>
       <p>Bad: {feedback.bad}</p>
+      <p>Total feedback: {totalFeedback}</p>
+      <p>Positive feedback: {positiveFeedbackPercentage}%</p>
     </div>
   );
 };
